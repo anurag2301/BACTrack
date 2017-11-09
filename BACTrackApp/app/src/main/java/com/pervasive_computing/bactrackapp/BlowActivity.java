9dc5eadc443bdf9a1c17eef2b3b11c0cc795d0ba
pratik11jain <pratikjain1104@gmail.com>
2017-11-09 11:22:02 -0500

    Added some screens

---
+++ BACTrackApp/app/src/main/java/com/pervasive_computing/bactrackapp/BlowActivity.java
@@ -0,0 +1,19 @@
+package com.pervasive_computing.bactrackapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+/*
+  Created by Pratik on 11/08/2017.
+ */
+
+public class BlowActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_blow);
+    }
+
+    public void blow_clicked(View view) {
+    }
+}
---
+++ BACTrackApp/app/src/main/java/com/pervasive_computing/bactrackapp/HowGoingHome.java
@@ -0,0 +1,34 @@
+package com.pervasive_computing.bactrackapp;
+
+import android.os.Bundle;
+import android.view.View;
+
+/*
+  Created by Pratik on 11/07/2017.
+ */
+
+public class HowGoingHome extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.how_going_home);
+    }
+
+    public void other_clicked(View view) {
+    }
+
+    public void taxi_clicked(View view) {
+    }
+
+    public void bus_clicked(View view) {
+    }
+
+    public void friends_clicked(View view) {
+    }
+
+    public void car_clicked(View view) {
+    }
+
+    public void walk_clicked(View view) {
+    }
+}
