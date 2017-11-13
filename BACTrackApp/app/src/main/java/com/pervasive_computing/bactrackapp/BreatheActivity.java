562c5360a9e71f752ea4c2f6b337756dcc440543
pratik11jain <pratikjain1104@gmail.com>
2017-11-13 13:48:50 -0500

    Fixed progress bar

--- BACTrackApp/app/src/main/java/com/pervasive_computing/bactrackapp/BreatheActivity.java
+++ BACTrackApp/app/src/main/java/com/pervasive_computing/bactrackapp/BreatheActivity.java
@@ -1,23 +1,40 @@
 package com.pervasive_computing.bactrackapp;
 
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
-import android.view.animation.DecelerateInterpolator;
+import android.os.Handler;
 import android.widget.ProgressBar;
 
-/**
- * Created by Pratik on 11/10/2017.
+/*
+  Created by Pratik on 11/10/2017.
  */
 
 public class BreatheActivity extends BaseActivity {
+    private ProgressBar progressBar;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.breathe);
-        ProgressBar progressBar = findViewById(R.id.progressBar);
-        ObjectAnimator animation = ObjectAnimator.ofInt(progressBar, "progress", 0, 500); // see this max value coming back here, we animale towards that value
-        animation.setDuration(5000); //in milliseconds
-        animation.setInterpolator(new DecelerateInterpolator());
-        animation.start();
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setSecondaryProgress(progressBar.getMax());
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < progressBar.getMax()) {
+                    progressStatus += 1;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                        }
+                    });
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
--- BACTrackApp/app/src/main/java/com/pervasive_computing/bactrackapp/ContactPicker.java
+++ BACTrackApp/app/src/main/java/com/pervasive_computing/bactrackapp/ContactPicker.java
@@ -1,8 +1,12 @@
 package com.pervasive_computing.bactrackapp;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
@@ -18,6 +22,11 @@ public class ContactPicker extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact_list);
+        startContactIntent();
+    }
+
+    private void startContactIntent() {
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
         startActivityForResult(intent, CONTACT_PICKER_RESULT);
@@ -42,11 +51,41 @@ public class ContactPicker extends BaseActivity {
                     name = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
                     cursor.close();
                 }
-                DBHelper dbInstance = new DBHelper(getApplicationContext());
-                dbInstance.insertContact(name, number);
+                addToDb(ContactPicker.this, name, number);
             }
         }
-        finish();
+        //finish();
+    }
+
+    private void addToDb(final Context context, final String name, final String number) {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
+        } else {
+            builder = new AlertDialog.Builder(context);
+        }
+        builder.setTitle("Add Contact").setCancelable(false)
+                .setMessage("Are you sure you would like to add: " + name + " to your reliable contact list?\n" +
+                        "\n" +
+                        "This person will receive notifications about your BAC level and evening plans in the future.")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        DBHelper dbInstance = new DBHelper(getApplicationContext());
+                        dbInstance.insertContact(name, number);
+                        finish();
+                        startActivity(new Intent(context, ListContacts.class));
+                    }
+                })
+                .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // do nothing
+                        dialog.dismiss();
+                        finish();
+                        startActivity(new Intent(context, ListContacts.class));
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
 
